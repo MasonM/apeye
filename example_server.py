6eb60062f8698651e95a1a4bbb9e6fc26b3b56ae
Mason Malone <mason.malone@gmail.com>
2012-10-20 19:03:46 -0400

    Update example_server.py to include a simple soap server

--- example_server.py
+++ example_server.py
@@ -1,31 +1,62 @@
+"""
+A simple Flask app that includes a JSON-RPC and SOAP server that both expose
+an "add" method.
+"""
 from flask import Flask, request, make_response
-import simplejsonrpc as jsonrpc
+import simplejsonrpc
+from pysimplesoap.server import SoapDispatcher
 
 app = Flask(__name__)
 
-class MyJsonrpcHandler(jsonrpc.JsonrpcHandler):
-	"""define your own dispatcher here"""
+def add_cors_headers(response):
+	response.headers['Access-Control-Allow-Origin'] = '*'
+	response.headers['Access-Control-Allow-Headers'] = "Content-Type, X-Requested-With, Authentication"
+	response.headers['Access-Control-Allow-Method'] = "POST, GET, OPTIONS, PUT, DELETE, TRACE"
+	return response
+
+def add(a, b):
+	return a + b
+
+class ExploRPCHandler(simplejsonrpc.JsonrpcHandler):
 	def dispatch(self, method_name):
-		if method_name == "add":
-			return lambda a,b: a+b
-		else:
-			return default
+		return add if (method_name == "add") else None
 
 @app.route("/jsonrpc",  methods=['GET', 'POST', 'OPTIONS'])
-def add():
+def jsonrpc():
 	result = ''
 	if request.headers["CONTENT_LENGTH"]:
-		h = MyJsonrpcHandler()
+		h = ExploRPCHandler()
 		data = request.data
 		result = h.handle(data)
 		#app.logger.debug("FOO |%s| |%s|" % (data, result))
 
 	resp = make_response(result, 200)
-	resp.headers['Access-Control-Allow-Origin'] = '*'
-	resp.headers['Access-Control-Allow-Headers'] = "Content-Type, X-Requested-With, Authentication"
-	resp.headers['Access-Control-Allow-Method'] = "POST, GET, OPTIONS, PUT, DELETE, TRACE"
+	return add_cors_headers(resp)
 
-	return resp
+
+dispatcher = SoapDispatcher(
+	'explorpc_dispatcher',
+	location = "http://api.explorpc.org/soap",
+	action = 'http://api.explorpc.org/', # SOAPAction
+	namespace = "http://api.explorpc.org/sample.wsdl", prefix="ns0",
+	trace = True,
+	ns = True)
+
+# register the user function
+dispatcher.register_function('Adder', add,
+	returns={'AddResult': int},
+	args={'a': int, 'b': int})
+
+@app.route("/soap",  methods=['GET', 'POST', 'OPTIONS'])
+def soap():
+	result = ''
+	if request.headers["CONTENT_LENGTH"]:
+		data = request.data
+		result = dispatcher.dispatch(data, request.headers["SOAPAction"])
+		app.logger.debug("FOO |%s| |%s|" % (data, result))
+
+	resp = make_response(result, 200)
+	return add_cors_headers(resp)
 
 @app.route("/")
 def index():
