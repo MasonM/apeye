20871f73bfdcb22bc3a44a80e48122917d5ab804
Mason Malone <mason.malone@gmail.com>
2012-11-05 20:48:22 -0500

    Cleaned example_server.py and change index.html to show a list of examples for each of the 3 protocols

--- example_server.py
+++ example_server.py
@@ -1,5 +1,5 @@
 """
-A simple Flask app that includes a JSON-RPC and SOAP server that both expose
+A simple Flask app that includes a JSON-RPC, XML-RPC and SOAP server that all expose
 an "add" method.
 """
 from flask import Flask, request, make_response
@@ -15,13 +15,19 @@ def add_cors_headers(response):
 	response.headers['Access-Control-Allow-Method'] = "POST, GET, OPTIONS, PUT, DELETE, TRACE"
 	return response
 
+
 def add(a, b):
 	return a + b
+
+
+xmlrpc_handler = XMLRPCHandler('xml-rpc')
+xmlrpc_handler.connect(app, '/xml-rpc')
+xmlrpc_handler.register(add)
+
 
 class ExploRPCHandler(simplejsonrpc.JsonrpcHandler):
 	def dispatch(self, method_name):
 		return add if (method_name == "add") else None
-
 @app.route("/jsonrpc",  methods=['GET', 'POST', 'OPTIONS'])
 def jsonrpc():
 	result = ''
@@ -30,9 +36,9 @@ def jsonrpc():
 		data = request.data
 		result = h.handle(data)
 		#app.logger.debug("FOO |%s| |%s|" % (data, result))
-
 	resp = make_response(result, 200)
 	return add_cors_headers(resp)
+
 
 dispatcher = SoapDispatcher(
 	'explorpc_dispatcher',
@@ -41,34 +47,29 @@ dispatcher = SoapDispatcher(
 	namespace = "http://api.explorpc.org/sample.wsdl", prefix="ns0",
 	trace = True,
 	ns = True)
-
-# register the user function
 dispatcher.register_function('Adder', add,
 	returns={'AddResult': int},
 	args={'a': int, 'b': int})
-
 @app.route("/soap",  methods=['GET', 'POST', 'OPTIONS'])
 def soap():
 	result = ''
 	if request.headers["CONTENT_LENGTH"]:
 		data = request.data
 		result = dispatcher.dispatch(data, request.headers["SOAPAction"])
-		app.logger.debug("FOO |%s| |%s|" % (data, result))
-
+		#app.logger.debug("FOO |%s| |%s|" % (data, result))
 	resp = make_response(result, 200)
 	return add_cors_headers(resp)
 
-xmlrpc_handler = XMLRPCHandler('xml-rpc')
-xmlrpc_handler.connect(app, '/xml-rpc')
-xmlrpc_handler.register(add)
 
 @app.route("/")
 def index():
 	return open('index.html').read()
 
+
 @app.route("/tunnel.html")
 def tunnel():
 	return open('tunnel.html').read()
+
 
 if __name__ == "__main__":
 	app.debug = True
