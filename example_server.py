dd22e457ec818aad14b2f6fccc920592068d5b83
Mason Malone <mason.malone@gmail.com>
2012-11-11 14:34:14 -0500

    Update example_server.py to include a lot more functions, mainly to show off autocomplete functionality better

--- example_server.py
+++ example_server.py
@@ -1,54 +1,73 @@
 """
 A simple Flask app that includes a JSON-RPC, XML-RPC and SOAP server that all expose
-an "add" method.
+a set of simple math functions (add, subtract, etc). Useful for testing.
 """
 from flask import Flask, request, make_response
 from flaskext.xmlrpc import XMLRPCHandler, Fault
 from werkzeug.wsgi import DispatcherMiddleware
 import simplejsonrpc
 import soaplib
+import math
 from soaplib.core.service import rpc, soap, DefinitionBase
-from soaplib.core.model.primitive import Integer
+from soaplib.core.model.clazz import Array
+from soaplib.core.model.primitive import Integer, Decimal
 from soaplib.core.server import wsgi
 
 app = Flask(__name__)
 
-def add_cors_headers(response):
+functions = {
+	'add': lambda a, b: a + b,
+	'subtract': lambda a, b: a - b,
+	'multiply': lambda a, b: a * b,
+	'divide': lambda a, b: a / b,
+	'square': lambda a: a * a,
+	'squareRoot': lambda a: math.sqrt(a),
+	'sum': lambda a: sum(a),
+	'average': lambda a: sum(a) / len(a),
+}
+
+xmlrpc_handler = XMLRPCHandler('xml-rpc')
+xmlrpc_handler.connect(app, '/xml-rpc')
+for name, func in functions.iteritems():
+	xmlrpc_handler.register(func, name)
+
+
+class JSONRPCHandler(simplejsonrpc.JsonrpcHandler):
+	def dispatch(self, method_name):
+		return functions.get(method_name, None);
+@app.route("/json-rpc", methods=['GET', 'POST', 'OPTIONS'])
+def jsonrpc():
+	result = ''
+	if request.headers["CONTENT_LENGTH"]:
+		h = JSONRPCHandler()
+		data = request.data
+		result = h.handle(data)
+	response = make_response(result, 200)
+	# add CORS headers
 	response.headers['Access-Control-Allow-Origin'] = '*'
 	response.headers['Access-Control-Allow-Headers'] = "Content-Type, X-Requested-With, Authentication"
 	response.headers['Access-Control-Allow-Method'] = "POST, GET, OPTIONS, PUT, DELETE, TRACE"
 	return response
 
 
-def add(a, b):
-	return a + b
-
-
-xmlrpc_handler = XMLRPCHandler('xml-rpc')
-xmlrpc_handler.connect(app, '/xml-rpc')
-xmlrpc_handler.register(add)
-
-
-class ExploRPCHandler(simplejsonrpc.JsonrpcHandler):
-	def dispatch(self, method_name):
-		return add if (method_name == "add") else None
-@app.route("/json-rpc", methods=['GET', 'POST', 'OPTIONS'])
-def jsonrpc():
-	result = ''
-	if request.headers["CONTENT_LENGTH"]:
-		h = ExploRPCHandler()
-		data = request.data
-		result = h.handle(data)
-		#app.logger.debug("FOO |%s| |%s|" % (data, result))
-	resp = make_response(result, 200)
-	return add_cors_headers(resp)
-
-
-class AddService(DefinitionBase):
-	@rpc(Integer,Integer,_returns=Integer)
-	def add(self,a,b):
-		 return a + b
-soap_application = soaplib.core.Application([AddService], 'tns')
+class SoapService(DefinitionBase):
+	@rpc(Decimal, Decimal, _returns=Decimal)
+	def add(self, a, b): return a + b
+	@rpc(Decimal, Decimal, _returns=Decimal)
+	def subtract(self, a, b): return a - b
+	@rpc(Decimal, Decimal, _returns=Decimal)
+	def multiply(self, a, b): return a * b
+	@rpc(Decimal, Decimal, _returns=Decimal)
+	def divide(self, a, b): return a / b
+	@rpc(Decimal, _returns=Decimal)
+	def square(self, a): return a * a
+	@rpc(Decimal, _returns=Decimal)
+	def squareRoot(self, a): return math.sqrt(a)
+	@rpc(Array(Decimal), _returns=Decimal)
+	def sum(self, a): return sum(a)
+	@rpc(Array(Decimal), _returns=Decimal)
+	def average(self, a): return sum(a) / len(a)
+soap_application = soaplib.core.Application([SoapService], 'tns')
 app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
 	'/soap': wsgi.Application(soap_application),
 })
