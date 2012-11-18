caed286a45c017db5486e3b5614902adb7a1690a
Mason Malone <mason.malone@gmail.com>
2012-11-18 01:10:15 -0500

    Add ability to generate permalinks for a request and response by serializing them and sending the data to an external server, then deserializing on page load. Updated example_server.py to add a simple MySQL-powered pastebin and changed index.html to use it for permalinks. Refactored explorpc.js request/response handling to facilitate serializing and deserializing

--- example_server.py
+++ example_server.py
@@ -1,16 +1,18 @@
 """
-A simple Flask app that includes a JSON-RPC, XML-RPC and SOAP server that all expose
-a set of simple math functions (add, subtract, etc). Useful for testing.
+A Flask app that exposes a common set of math functions via JSON-RPC 2.0,
+XML-RPC and SOAP 1.1 using the simplejsonrpc, Flask-XML-RPC, and soaplib
+libraries, respectively.
+
+Also implements a very simple pastebin that can be used for exploRPC's permanent
+link functionality by storing the serialized requests and responses.
 """
+import simplejsonrpc, soaplib, MySQLdb, math, hashlib
 from flask import Flask, request, make_response
-from flaskext.xmlrpc import XMLRPCHandler, Fault
+from flaskext.xmlrpc import XMLRPCHandler
 from werkzeug.wsgi import DispatcherMiddleware
-import simplejsonrpc
-import soaplib
-import math
-from soaplib.core.service import rpc, soap, DefinitionBase
+from soaplib.core.service import rpc, DefinitionBase
 from soaplib.core.model.clazz import Array
-from soaplib.core.model.primitive import Integer, Decimal
+from soaplib.core.model.primitive import Decimal
 from soaplib.core.server import wsgi
 
 app = Flask(__name__)
@@ -42,9 +44,10 @@ def jsonrpc():
 		result = handler.handle(request.data)
 	response = make_response(result, 200)
 	# add CORS headers
-	response.headers['Access-Control-Allow-Origin'] = '*'
-	response.headers['Access-Control-Allow-Headers'] = "Content-Type, X-Requested-With, Authentication"
-	response.headers['Access-Control-Allow-Method'] = "POST, GET, OPTIONS, PUT, DELETE, TRACE"
+	hdr = 'Access-Control-Allow-%s'
+	response.headers[hdr % 'Origin'] = '*'
+	response.headers[hdr % 'Headers'] = "Content-Type, Authentication"
+	response.headers[hdr % 'Method'] = "POST, GET, OPTIONS, PUT, DELETE, TRACE"
 	return response
 
 class SoapService(DefinitionBase):
@@ -68,6 +71,33 @@ soap_application = soaplib.core.Application([SoapService], 'tns')
 app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
 	'/soap': wsgi.Application(soap_application),
 })
+
+@app.route("/pastebin", methods=["GET", "POST"])
+def pastebin():
+	db = MySQLdb.connect(host='localhost', user='explorpc', db='example_server')
+	result = ''
+	if request.method == "POST" and request.headers['CONTENT_LENGTH']:
+		result = insert_entry(db.cursor(), request.data)
+		db.commit()
+	elif request.method == "GET":
+		print request.args['id']
+		result = get_entry(db.cursor(), request.args['id'])
+	return make_response(unicode(result), 200)
+def get_entry(cursor, entry_id):
+	cursor.execute("SELECT entry FROM entries WHERE id = %s", (entry_id,))
+	row = cursor.fetchone()
+	return row[0] if row else ""
+def insert_entry(cursor, entry):
+	content_hash = hashlib.sha1(entry).hexdigest()
+	if not get_entry(cursor, content_hash):
+		cursor.execute("INSERT INTO entries (id, entry, created_time) VALUES (%s, %s, NOW())", (content_hash, entry));
+	return content_hash
+
+@app.route("/")
+def index(): return open('index.html').read()
+
+@app.route("/tunnel.html")
+def tunnel(): return open('tunnel.html').read()
 
 if __name__ == "__main__":
 	app.debug = True
