042159ac89ea0866c58cf171893ade25a6c31e77
Mason Malone <mason.malone@gmail.com>
2013-01-26 20:43:21 -0500

    Fixing some lines I forgot to change when renaming the RPC example JS file. Also fixed REST console so it display properly when initialized

--- docs/js/apeye-rest-example.js
+++ docs/js/apeye-rest-example.js
@@ -1,25 +1,23 @@
 $(function() {
-	$('#apeye-rest-example').apeye({
-		// FIELD SETTERS: Auto-fill the fields with a sample request
-		'url': 'api.apeye.org/rest/book/<book_id>',
-		'type': 'raw',
-		'httpMethod': 'get',
-
-		// OPTIONS
-		// Automatically pretty-print responses
-		'autoPrettyPrint': true,
-		// Always tunnel requests through an iframe (needed because the endpoint in the sample request is on a different subdomain)
-		'subdomainTunneling': true,
-		'autocompleteUrlSource': [
-			'api.apeye.org/rest/book',
-			'api.apeye.org/rest/book/<book_id>',
-			'api.apeye.org/rest/author',
-			'api.apeye.org/rest/author/<author_id>'
-		]
-	});
-
 	$('#apeye-launch-rest-example').on('click', function(event) {
-		$('#apeye-rest-example').show();
+		$('#apeye-rest-example').show().apeye({
+			// FIELD SETTERS: Auto-fill the fields with a sample request
+			'url': 'api.apeye.org/rest/book/<book_id>',
+			'type': 'raw',
+			'httpMethod': 'get',
+
+			// OPTIONS
+			// Automatically pretty-print responses
+			'autoPrettyPrint': true,
+			// Always tunnel requests through an iframe (needed because the endpoint in the sample request is on a different subdomain)
+			'subdomainTunneling': true,
+			'autocompleteUrlSource': [
+				'api.apeye.org/rest/book',
+				'api.apeye.org/rest/book/<book_id>',
+				'api.apeye.org/rest/author',
+				'api.apeye.org/rest/author/<author_id>'
+			]
+		});
 		$(this).hide();
 		event.preventDefault();
 	});
